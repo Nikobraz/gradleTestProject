08d3aa813f73e3015d156ba976bab97b6cc2206f
nikobraz <nikita_yarushin@mail.ru>
2021-01-25 15:50:19 +0300

    Add Allure

--- src/test/java/ru/nikobraz/gradleTestProject/CalcTest.java
+++ src/test/java/ru/nikobraz/gradleTestProject/CalcTest.java
@@ -1,32 +1,36 @@
 package ru.nikobraz.gradleTestProject;
 
-import org.junit.Test;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
-
-import ru.nikobraz.gradleTestProject.Calc;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class CalcTest {
 
     @Test
+    @Owner("Ярушин Никита")
     public void sum() {
         assertEquals(4, Calc.sum(2,2));
         assertNotEquals(5, Calc.sum(2,2));
     }
 
     @Test
+    @Owner("Ярушин Никита")
     public void sub() {
         assertEquals(4, Calc.sub(6,2));
         assertNotEquals(5, Calc.sub(2,2));
     }
 
     @Test
+    @Owner("Ярушин Никита")
     public void mul() {
         assertEquals(8, Calc.mul(4,2));
         assertNotEquals(5, Calc.mul(2,2));
     }
 
     @Test
+    @Owner("Ярушин Никита")
     public void div() {
         assertEquals(3, Calc.div(6,2));
         assertNotEquals(5, Calc.div(26,2));
